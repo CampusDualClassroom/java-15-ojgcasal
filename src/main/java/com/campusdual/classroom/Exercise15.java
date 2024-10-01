195391a06276aed035f0039ff1b2b6e7b18d9525
Orlando Jose Garces Casal <ojgcasal@gmail.com>
2024-10-01 11:23:16 +0200

    Listo

--- src/main/java/com/campusdual/classroom/Exercise15.java
+++ src/main/java/com/campusdual/classroom/Exercise15.java
@@ -3,13 +3,14 @@ package com.campusdual.classroom;
 import java.util.Calendar;
 
 public class Exercise15 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		FreshMerchandise fm = new FreshMerchandise("Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53,
-				Calendar.getInstance().getTime());
-		fm.printSpecificData();
-		System.out.println(fm.getLocation());
+        FreshMerchandise fm = new FreshMerchandise(
+                "Manzanas", "001-9", "Frutería de ayer S.L.", 8, "C", "114D", 53,
+                Calendar.getInstance().getTime()
+        );
 
-	}
+        fm.printSpecificData();
 
+    }
 }
